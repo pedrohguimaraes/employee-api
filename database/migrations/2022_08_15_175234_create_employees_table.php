2f47c6768d66bedea3a2f241a7bba5105fb4c3a6
Pedro Guimaraes <pedroguimarraes@gmail.com>
2022-08-15 22:41:29 -0300

    tables migrations

---
+++ database/migrations/2022_08_15_175234_create_employees_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cpf');
+            $table->string('rg');
+            $table->string('email');
+            $table->date('birth_date');
+            $table->string('cep');
+            $table->string('address');
+            $table->string('number');
+            $table->string('city');
+            $table->string('state');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
